ce00dac48a1849f2bab3e89e71ebc8cd2149060a
Brian Pham <brian.pham@carro.co>
2022-05-29 14:40:25 -0500

    update hour

--- modules/homePage/components/ContactContent.js
+++ modules/homePage/components/ContactContent.js
@@ -1,7 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useCoreServices } from '../../core';
+
+const reader = require('g-sheets-api');
 
 const ContactContent = () => {
+    const [hourData, setHourData] = useState([]);
+    const { getSheetOptions } = useCoreServices();
+
+    const hourOptions = getSheetOptions('hour');
+
+    useEffect(() => {
+        reader(hourOptions, results => {
+            setHourData(results);
+        });
+    }, []);
+
     return (
         <div style={{ marginBottom: 50 }}>
             <div className="is-page-title" id="contact">
@@ -10,8 +25,9 @@ const ContactContent = () => {
             <div className="columns" style={{ fontSize: 27 }}>
                 <div className="column has-text-center">
                     <div className="is-description">Hours:</div>
-                    <div>Monday: Closed</div>
-                    <div>Tuesday - Sunday: 11:00am to 7:00pm</div>
+                    {hourData.map(item => (
+                        <div>{item.title}</div>
+                    ))}
                     <div className="is-description">Address:</div>
                     <div>Unit - A 2630 Quadra Street</div>
                     <div>Victoria, BC V8T 4E4</div>
--- modules/homePage/components/NotiContent.js
+++ modules/homePage/components/NotiContent.js
@@ -3,9 +3,9 @@ import React from 'react';
 const NotiContent = ({ notiData }) => {
     return (
         <div id="noti" className="container">
-            {notiData.map((item, index) => (
+            {notiData?.map((item, index) => (
                 <div key={index} className="is-page-title" style={{ fontSize: 45 }}>
-                    {item.name}
+                    {item.title}
                 </div>
             ))}
         </div>
